e7e76afcf14aa41b2b11501ab15389500de8a7ad
rafaelamaral2-netizen <rafaelamaral2@icloud.com>
2026-04-19 12:06:13 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("JS conectado correctamente");
+document.getElementById("app").innerHTML = `
+  <h1 style="text-align:center;margin-top:40px;color:#61d87d;">
+    EARNX CORE LOADED
+  </h1>
+`;
